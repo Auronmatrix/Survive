e8d0bf2930afe03fd2cee03e1e5675cdc66fe5ab
auronmatrix <aubrey.oosthuizen@ob7.co.za>
2013-11-22 14:03:17 +0100

    Revert "Revert "Changed methods using flush in GenericJpaDao""
    This reverts commit d678457d17b61ea4cd465678a7fd900ea6c0fa4e.

--- Survive-Modules/Survive-Java-Core/src/main/java/com/muni/fi/pa165/dao/commons/GenericJpaDao.java
+++ Survive-Modules/Survive-Java-Core/src/main/java/com/muni/fi/pa165/dao/commons/GenericJpaDao.java
@@ -7,12 +7,14 @@ package com.muni.fi.pa165.dao.commons;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  *
  * @author Aubrey Oosthuizen Abstract class used for implementing CRUD operations on generic types. Used to decouple
  * data access and persistence from business layer. Will be extended by each DAO object class for every entity type
  */
+
 public abstract class GenericJpaDao<T, ID> implements GenericDao<T, ID> {
 
     private Class<T> persistentClass;
@@ -36,6 +38,7 @@ public abstract class GenericJpaDao<T, ID> implements GenericDao<T, ID> {
     }
 
     @Override
+    @Transactional
     public T save(T entity) {
 
         //not necessary - entityManager.persist(null) throws the same exception
@@ -43,8 +46,7 @@ public abstract class GenericJpaDao<T, ID> implements GenericDao<T, ID> {
 //            throw new IllegalArgumentException("entity to be created is null");
 //        }
 
-        entityManager.persist(entity);
-        flush();
+        entityManager.persist(entity);       
         return entity;
     }
 
@@ -55,17 +57,21 @@ public abstract class GenericJpaDao<T, ID> implements GenericDao<T, ID> {
     }
 
     @Override
+    @Transactional
     public void delete(T entity) {
-        getEntityManager().merge(entity);
-        getEntityManager().remove(entity);
+     entityManager.remove(entityManager.contains(entity) ? entity : entityManager.merge(entity));
     }
     
-    @Override
+    @Override   
+    @Transactional
     public void delete(Long id) {
-        flush();
-        T entity = (T) getEntityManager().find(getPersistentClass(), id);
-        getEntityManager().remove(entity);
-    }
+    
+        T entity =  entityManager.getReference(getPersistentClass(), id);
+       entityManager.remove(entity);
+   }
+    
+       //  entityManager.remove(entityManager.contains(entity) ? entity : entityManager.merge(entity));
+   
 
     @Override
     public T findById(ID id) {
