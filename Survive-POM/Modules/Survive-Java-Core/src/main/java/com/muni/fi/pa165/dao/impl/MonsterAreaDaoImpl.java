d228462454394a81d0c193840b6c15496c9fa097
PETA <PETA@PETA>
2013-11-26 22:08:54 +0100

    log4j conf

--- Survive-POM/Modules/Survive-Java-Core/src/main/java/com/muni/fi/pa165/dao/impl/MonsterAreaDaoImpl.java
+++ Survive-POM/Modules/Survive-Java-Core/src/main/java/com/muni/fi/pa165/dao/impl/MonsterAreaDaoImpl.java
@@ -9,8 +9,11 @@ import com.muni.fi.pa165.dao.MonsterAreaDao;
 import com.muni.fi.pa165.dao.gen.GenericDaoAbs;
 import com.muni.fi.pa165.entities.Monsterarea;
 import com.muni.fi.pa165.entities.MonsterareaPK;
+import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 import org.springframework.stereotype.Repository;
 
@@ -29,22 +32,107 @@ public class MonsterAreaDaoImpl extends GenericDaoAbs<Monsterarea, Long> impleme
     }
 
     @Override
-    public List<Monsterarea> getByMonsterId(Long id){
- EntityManager em = this.getEntityManagerFactory().createEntityManager();
-        TypedQuery<Monsterarea> query = em.createQuery("select m from " + getPersistentClass().getSimpleName()  + " m where m.monster.id = :id", Monsterarea.class);
-        query.setParameter("id", id);
-       return query.getResultList();
+    public List<Monsterarea> getByMonsterId(Long id) {
+        EntityManager em = this.getEntityManagerFactory().createEntityManager();
+        Query query = em.createNamedQuery("Monsterarea.findByMonsterid").setParameter("monsterid", id);
+        EntityTransaction tx = em.getTransaction();
+        List <Monsterarea> list = new ArrayList();
+        try {
+                tx.begin();
+                list = query.getResultList();
+                tx.commit();        
+        }
+        finally {
+            
+            if (em != null) {
+                em.close();
+            }
+        }
+       return list;
     }
 
     @Override
     public List<Monsterarea> getByAreaId(Long id) {
         EntityManager em = this.getEntityManagerFactory().createEntityManager();
-           TypedQuery<Monsterarea> query = em.createQuery("select m from " + getPersistentClass().getSimpleName()  + " m where m.weapon.id = :id", Monsterarea.class);
-        query.setParameter("id", id);
-       return query.getResultList(); 
+        Query query = em.createNamedQuery("Monsterarea.findByAreaid").setParameter("areaid", id);
+        EntityTransaction tx = em.getTransaction();
+        List <Monsterarea> list = new ArrayList();
+        try {
+                tx.begin();
+                list = query.getResultList();
+                tx.commit();        
+        }
+        finally {
+            
+            if (em != null) {
+                em.close();
+            }
+        }
+       return list;
+    
+    }
+    
+    @Override
+    public void delete(MonsterareaPK pk)
+    {    
+        EntityManager em = this.getEntityManagerFactory().createEntityManager();
+        EntityTransaction tx = em.getTransaction(); 
+        try {
+                tx.begin();
+                Monsterarea entity = findById(pk);
+                em.merge(pk);
+               em.merge(entity);
+               em.remove(entity);
+               tx.commit();
+               }
+        finally {
+            if (em != null) {
+                em.close();
+            }
+        }
     }
 
-
+    @Override
+    public List<Monsterarea> findAll() {
+        EntityManager em = this.getEntityManagerFactory().createEntityManager();      
+        Query query = em.createNamedQuery("Monsterarea.findAll");
+        EntityTransaction tx = em.getTransaction();
+        List <Monsterarea> list = new ArrayList();
+        try {
+                tx.begin();
+                list = query.getResultList();
+                tx.commit();        
+        }
+        finally {
+            
+            if (em != null) {
+                em.close();
+            }
+        }
+       return list;
+     }
+    
+    @Override
+    public Monsterarea findById(MonsterareaPK id) {
+      EntityManager em = this.getEntityManagerFactory().createEntityManager();      
+        Query query = em.createNamedQuery("Monsterarea.findById");
+        query.setParameter("MonsterareaPK", id);
+        EntityTransaction tx = em.getTransaction();
+        Monsterarea obj = null;
+        try {
+                tx.begin();
+                obj = (Monsterarea) query.getSingleResult();
+                tx.commit();        
+        }
+        finally {
+            
+            if (em != null) {
+                em.close();
+            }
+        }
+       return obj;
+        
+    }
 }
 
     
