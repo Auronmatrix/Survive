d92aa8b55423018fd3cffd6e63b7f5bc03752e89
Michal <vinklermichal@gmail.com>
2013-12-07 16:39:11 +0100

    REST implementation - simple test - path /rest/weapons/  v.2

--- Survive-POM/Survive-Web/src/main/java/com/muni/fi/pa165/rest/WeaponsResource.java
+++ Survive-POM/Survive-Web/src/main/java/com/muni/fi/pa165/rest/WeaponsResource.java
@@ -6,28 +6,64 @@
 
 package com.muni.fi.pa165.rest;
 
+import com.muni.fi.pa165.dto.WeaponDto;
+import com.muni.fi.pa165.service.WeaponService;
+import java.util.Collection;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.GenericEntity;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 /**
  *
  * @author xvinkle1
  */
+
 @Path("weapons")
 @Component
-public class WeaponsResource {
+public class WeaponsResource implements EntityResource {
+    
+//    @Inject
+    @Autowired
+    WeaponService weaponService;
     
     @GET
     @Produces(MediaType.TEXT_PLAIN)
+    @Override
     public String getPlain() {
         String test = "test";
 
         
 
         return test;
+    }
+    
+    
+    @GET
+    @Path("/all")
+    @Produces({MediaType.APPLICATION_JSON})
+    @Override
+    public Response getAll() {
+        
+        if(weaponService == null) {
+            System.out.println("WEAPON SERVICE: NULL" );
+        }
+       
+        try {
+            Collection<WeaponDto> weaponsDto = weaponService.findAll();
+
+            
+            GenericEntity<Collection<WeaponDto>> ge =
+                    new GenericEntity<Collection<WeaponDto>>(weaponsDto) {
+                    };
+            return Response.ok().entity(ge).build();
+        } catch (Exception ex) {
+            return Response.status(Response.Status.CONFLICT).build();
+        }
     }
     
 }
