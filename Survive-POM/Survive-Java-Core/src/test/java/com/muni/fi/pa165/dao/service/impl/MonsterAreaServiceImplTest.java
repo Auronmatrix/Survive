b163810cf90cf9f4f886e3699cddfb30ddd88c2c
Irina <irina@irishka>
2013-12-11 23:14:03 +0100

    fixed tests in MonsterAreaServiceImpl

--- Survive-POM/Survive-Java-Core/src/test/java/com/muni/fi/pa165/dao/service/impl/MonsterAreaServiceImplTest.java
+++ Survive-POM/Survive-Java-Core/src/test/java/com/muni/fi/pa165/dao/service/impl/MonsterAreaServiceImplTest.java
@@ -2,8 +2,10 @@ package com.muni.fi.pa165.dao.service.impl;
 
 import com.muni.fi.pa165.service.impl.MonsterAreaServiceImpl;
 import com.muni.fi.pa165.dao.MonsterAreaDao;
+import com.muni.fi.pa165.dto.AreaDto;
 import com.muni.fi.pa165.service.AbstractServiceIntegrationTest;
 import com.muni.fi.pa165.dto.MonsterAreaDto;
+import com.muni.fi.pa165.dto.MonsterDto;
 import com.muni.fi.pa165.entities.Area;
 import com.muni.fi.pa165.entities.Monster;
 import com.muni.fi.pa165.entities.Monsterarea;
@@ -15,10 +17,10 @@ import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.Test;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.*;
+
+import static org.junit.Assert.*;
+
 
 /**
  *
@@ -31,9 +33,9 @@ public class MonsterAreaServiceImplTest extends AbstractServiceIntegrationTest {
     private MonsterAreaServiceImpl service;
     @Inject
     private Mapper mapper;
-    private Monster monster = new Monster();
-    private Area area = new Area();
-    private MonsterAreaDto monsterAreaDto = new MonsterAreaDto();
+    private MonsterDto monsterDto;
+    private AreaDto areaDto;
+    private MonsterAreaDto monsterAreaDto;
 
     @Before
     public void setUp() {
@@ -42,24 +44,29 @@ public class MonsterAreaServiceImplTest extends AbstractServiceIntegrationTest {
         service.setDao(mockDAO);
         service.setMapper(mapper);
 
-        monster.setId(1L);
-        monster.setAgility(11.0);
-        monster.setDangerlevel(22.2);
-        monster.setDescription("Headless Zombie");
-        monster.setHeight(11.4);
-        monster.setImagepath("C:\\image.png");
-        monster.setMonsterclass(MonsterClass.Zombie);
-        monster.setStamina(11.5);
-        monster.setStrength(11.8);
-        monster.setWeight(11.2);
-        monster.setName("HeadlessNick");
-        monster.setId(Long.MIN_VALUE);
+        monsterDto = new MonsterDto();
+        monsterDto.setId(1L);
+        monsterDto.setAgility(11.0);
+        monsterDto.setDangerLevel(22.2);
+        monsterDto.setDescription("Headless Zombie");
+        monsterDto.setHeight(11.4);
+        monsterDto.setImagePath("C:\\image.png");
+        monsterDto.setMonsterClass(MonsterClass.Zombie);
+        monsterDto.setStamina(11.5);
+        monsterDto.setStrength(11.8);
+        monsterDto.setWeight(11.2);
+        monsterDto.setName("HeadlessNick");
+        monsterDto.setId(Long.MIN_VALUE);
+        
+        areaDto = new AreaDto();
+        areaDto.setName("Farm");
+        areaDto.setTerrain(TerrainType.SNOW);
+        areaDto.setId(Long.MIN_VALUE);
 
-        area.setName("Farm");
-        area.setTerrain(TerrainType.SNOW);
-        area.setId(Long.MIN_VALUE);
 
-
+        monsterAreaDto = new MonsterAreaDto();
+        monsterAreaDto.setArea(areaDto);
+        monsterAreaDto.setMonster(monsterDto);
         monsterAreaDto.setMonsterQuantity(5);
     }
 
@@ -76,11 +83,10 @@ public class MonsterAreaServiceImplTest extends AbstractServiceIntegrationTest {
      */
     @Test
     public void testSave() {
-//        Monsterarea entity = mapper.map(monsterAreaDto, Monsterarea.class);
-//        when(mockDAO.save(any(Monsterarea.class))).thenReturn(entity);
-//        MonsterAreaDto returned = service.save(monsterAreaDto);
-//        verify(mockDAO, times(1)).save(entity);
-//        assertEquals(returned, monsterAreaDto);
+        Monsterarea entity = mapper.map(monsterAreaDto, Monsterarea.class);
+        when(mockDAO.save(any(Monsterarea.class))).thenReturn(entity);
+        MonsterAreaDto returned = service.save(monsterAreaDto);
+        assertEquals(returned, monsterAreaDto);
     }
 
     /**
@@ -88,12 +94,10 @@ public class MonsterAreaServiceImplTest extends AbstractServiceIntegrationTest {
      */
     @Test
     public void testUpdate() {
-//        Monsterarea entity = mapper.map(monsterAreaDto, Monsterarea.class);
-//        when(mockDAO.update(any(Monsterarea.class))).thenReturn(entity);
-//        MonsterAreaDto returned = service.update(monsterAreaDto);
-//        verify(mockDAO, times(1)).update(entity);
-//        verifyNoMoreInteractions(mockDAO);
-//        assertEquals(returned, monsterAreaDto);
+        Monsterarea entity = mapper.map(monsterAreaDto, Monsterarea.class);
+        when(mockDAO.update(any(Monsterarea.class))).thenReturn(entity);
+        MonsterAreaDto returned = service.update(monsterAreaDto);
+        assertEquals(returned, monsterAreaDto);
     }
 
     /**
